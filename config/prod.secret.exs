4fb60cbdc63b64cc0e981c649be93c4fae66269c
Benjamin Scholtz <contact@benjaminscholtz.com>
2019-10-26 19:18:10 +0200

    Configure production server

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -32,7 +32,7 @@ config :example, ExampleWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :example, ExampleWeb.Endpoint, server: true
+config :example, ExampleWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
